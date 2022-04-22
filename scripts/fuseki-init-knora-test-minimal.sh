312016ccbebffa7bac66ca6554fd9848a38111c7
Loïc Jaouen <jaouen@bluewin.ch>
2022-04-22 15:11:35 +0200

    adding default onto and data

--- scripts/fuseki-init-knora-test-minimal.sh
+++ scripts/fuseki-init-knora-test-minimal.sh
@@ -8,10 +8,10 @@ REPOSITORY="knora-test"
 
 # delete-repository // delete dos not work correctly. need to delete database manually.
 create-repository
-upload-graph ../../knora-ontologies/knora-admin.ttl http://www.knora.org/ontology/knora-admin
-upload-graph ../../knora-ontologies/knora-base.ttl http://www.knora.org/ontology/knora-base
-upload-graph ../../knora-ontologies/standoff-onto.ttl http://www.knora.org/ontology/standoff
-upload-graph ../../knora-ontologies/standoff-data.ttl http://www.knora.org/data/standoff
-upload-graph ../../knora-ontologies/salsah-gui.ttl http://www.knora.org/ontology/salsah-gui
-upload-graph ../../test_data/all_data/admin-data-minimal.ttl http://www.knora.org/data/admin
-upload-graph ../../test_data/all_data/permissions-data-minimal.ttl http://www.knora.org/data/permissions
+upload-graph ../knora-ontologies/knora-admin.ttl http://www.knora.org/ontology/knora-admin
+upload-graph ../knora-ontologies/knora-base.ttl http://www.knora.org/ontology/knora-base
+upload-graph ../knora-ontologies/standoff-onto.ttl http://www.knora.org/ontology/standoff
+upload-graph ../knora-ontologies/standoff-data.ttl http://www.knora.org/data/standoff
+upload-graph ../knora-ontologies/salsah-gui.ttl http://www.knora.org/ontology/salsah-gui
+upload-graph ../test_data/all_data/admin-data-minimal.ttl http://www.knora.org/data/admin
+upload-graph ../test_data/all_data/permissions-data-minimal.ttl http://www.knora.org/data/permissions
